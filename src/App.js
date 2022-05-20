a7d611fa43f7baca7dff15a91ec9212705c372e9
Carrie Miskin <clove40@gmail.com>
2022-05-20 19:12:54 -0400

    added login and out

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './components/Home'
@@ -7,6 +7,8 @@ import Feed from './components/profile/Feed'
 import UserProfile from './components/profile/UserProfile'
 import Login from './components/session/Login'
 import Signup from './components/session/Signup'
+import Logout from './components/session/Logout'
+import { baseUrl } from './Globals'
 
 const App = () => {
   const [ currentUser, setCurrentUser ] = useState({})
@@ -15,16 +17,33 @@ const App = () => {
   const loginUser = user => {
     setCurrentUser(user);
     setLoggedIn(true);
+    localStorage.setItem('user_id', user.id);
   }
+
+  const logoutUser = () => {
+    setCurrentUser({});
+    setCurrentUser(false)
+    localStorage.removeItem('user_id')
+  }
+
+  useEffect(() => {
+    const userId = localStorage.getItem('user_id');
+    if(userId && !loggedIn) {
+      fetch(baseUrl + '/users' + userId)
+      .then(resp => resp.json())
+      .then(data => loginUser(data))
+
+    }
+  }, [])
 
   return (
     <Router>
-    <Navbar />
-    {loggedIn ? <h1>logged in!</h1> : null }
+    <Navbar loggedIn={ loggedIn } logoutUser={ logoutUser } />
       <Routes>
         <Route path="/" element ={<Home />} />
         <Route path="/signup" element={<Signup loginUser={ loginUser } />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
         <Route path="/profile" element={<UserProfile/>} />
         <Route path="/feed" element={<Feed />} />
       </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,25 @@ import './stylesheets/Navbar.css'
 import { NavLink } from 'react-router-dom'
 
 
-const Navbar = () => {
+const Navbar = ({ loggedIn, logoutUser }) => {
 
-  return (
-<div id="Navbar">
-    <NavLink to="/"> 
-        <img src={require("../components/assets/logo-01.png")} alt="logo" id='logo'/> 
-    </NavLink>
+  const logout = e => {
+    e.preventDefault();
+    logoutUser();
+  }
+
+const loggedInLinks = () => {
+  return(
+    <ul>
+        <li><NavLink to="/profile">My Profile</NavLink></li>
+        <li><NavLink to="/feed">Find My Dog</NavLink></li>
+        <li><a href="#" onClick={ logout }>Logout</a></li>
+    </ul>
+  )
+}
+
+const loggedOutLinks = () => {
+  return(
     <ul>
         <li>
           <NavLink to="/signup">Create Account</NavLink>
@@ -18,6 +30,13 @@ const Navbar = () => {
           <NavLink to="/login">Login</NavLink>
         </li>
     </ul>
+  )
+}
+
+  return (
+<div id="Navbar">
+    <NavLink to="/"><img src={require("../components/assets/logo-01.png")} alt="logo" id='logo'/></NavLink>
+    { loggedIn ? loggedInLinks() : loggedOutLinks() }
 </div>
   )
 }
