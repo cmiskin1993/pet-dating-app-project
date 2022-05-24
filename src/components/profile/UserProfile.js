d8506d46400077f374ae7256c8c105687012efe4
Carrie Miskin <clove40@gmail.com>
2022-05-24 19:55:00 -0400

    added profile infomation from server and styling

--- src/components/profile/UserProfile.js
+++ src/components/profile/UserProfile.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import '../stylesheets/UserProfile.css'
 
-const UserProfile = () => {
+
+const UserProfile = ({ currentUser }) => {
+
   return (
-    <div>UserProfile</div>
+    <section>
+      <div className="container">
+
+        <div className="photo_container">
+            <img className='profile_photo'  src={currentUser.formInputs.image} alt="profile_photo" />
+        </div>
+
+            <div className='header'>
+                <h2>{currentUser.formInputs.firstName} {currentUser.formInputs.lastName} </h2>
+            </div>
+
+            <div className="bio_container">
+              <h3>About Me </h3>
+                <p>{currentUser.formInputs.bio}</p> 
+            </div>
+            
+      </div>
+    </section>
+
+    
+
+    
   )
 }
 
